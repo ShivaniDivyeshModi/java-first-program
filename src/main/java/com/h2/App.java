d75a734e45b651e98be68ebab6178780778f8598
Modi <shivani.b.modi@accenture.com>
2021-08-31 17:18:09 -0400

    step 2 changes

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -16,7 +16,7 @@ public class App
 
     private static int add(int[] numbers) {
         int sum = 0;
-        for (int i = 0; !(i < numbers.length()); i++) {
+        for (int i = 0; i < numbers.length(); i++) {
             sum = sum + numbers[1];
         }
         return sum;
