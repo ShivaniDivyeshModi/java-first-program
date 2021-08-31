50ac84d2fb3707b9b7be5bfdde032a76ab267590
Modi <shivani.b.modi@accenture.com>
2021-08-31 17:08:45 -0400

    Step 2 Changes

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -4,14 +4,22 @@ package com.h2;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number*2;
     }
+
+    private static int add(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; !(i < numbers.length()); i++) {
+            sum = sum + numbers[1];
+        }
+        return sum;
+    }
+
 }
